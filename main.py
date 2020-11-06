cfb560843f221849b0d87271fa7ee80122731012
BrandonArnold1 <73853057+BrandonArnold1@users.noreply.github.com>
2020-11-06 10:10:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Brandon Arnold
+Period/Core: 3
 
+"""
 
+"""
+QUESTIONS
+
+Name A Toy
+Name A Place
+Name An Adjective
+Name A Verb Ending In Ing
+Name An Article Of Clothing
+Name A Color
+Name A Noun
+"""
+
+"""
+STORY
+
+I was in my room playing with my [TOY] and then my mom came in. She asked me if I wanted to go to [PLACE]. I was super [ADJECTIVE] and [VERB ENDING IN ING] up and ran to put on my
+[ARTICLE OF CLOTHING]. I jumped in the car with my [COLOR] jeans and put on my [NOUN].
 """
