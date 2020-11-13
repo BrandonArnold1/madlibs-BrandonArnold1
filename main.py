5828b3491601b0255dd1b6e55815edec03422f14
BrandonArnold1 <73853057+BrandonArnold1@users.noreply.github.com>
2020-11-13 13:18:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,40 @@ Period/Core:
 
 
 """
+"""
+STORY
+I was [VERB-ING] at the [PLACE] the other day when I heard [CHRISTMAS SONG] come on the radio. I was feeling really [ADJECTIVE] and I’m pretty sure I looked like it because 
+[FRIEND'S NAME] walked in and almost [VERB-ED]. Seriously, she [SAME VERB-ED] for close to [AMOUNT OF TIME]! This time every year I [VERB] all the time because I get really 
+exciting thinking about holiday [PLURAL NOUN]! That’s why I was sitting at the [SAME PLACE FROM BEFORE]: I was really hoping [RELATIVE'S NAME] would come by and give me at 
+least [NUMBER] pieces of holiday [FOOD ITEM]! That didn’t happen, but suddenly [SAME RELATIVE'S NAME] [VERB] by in a huge [NOUN] on wheels dressed up as [CELEBRITY]. I know 
+[ADJECTIVE] things happen around the holidays, but this didn’t make any sense! I picked up the phone to call [FRIEND'S NAME], but the phone turned into a big pile of 
+[MESSY FOOD] and dripped all the way down to my [BODY PART]! Then my alarm clock went off for about [AMOUNT OF TIME]. It was all a dream! I had a ton of [HOLIDAY FOOD] before 
+I went to sleep. Maybe that’s what gave me the crazy dream!
+"""
+"""
+WORDS
+
+Verb ending in ING: 
+Place: 
+Christmas song: 
+Adjective: 
+Friend's name:
+Verb ending in ED: 
+(Same verb ending in ED from before): 
+Amount of time: 
+Verb: 
+Plural Noun: 
+(Same place from before): 
+Relative's Name: 
+Number: 
+Food item: 
+(Same relative's name from before): 
+Verb: 
+Noun: 
+Celebrity: 
+Adjective: 
+Friend's name: 
+Messy food: 
+Body part: 
+Amount of time: 
+Holiday food: 
